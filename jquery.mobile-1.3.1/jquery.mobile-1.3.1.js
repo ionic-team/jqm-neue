e1bdde4d9d92dd05b46eaae76c5d03d026b5ba5e
Max Lynch <max@drifty.com>
2013-06-11 10:47:24 -0500

    Tweaking fast click

--- jquery.mobile-1.3.1/jquery.mobile-1.3.1.js
+++ jquery.mobile-1.3.1/jquery.mobile-1.3.1.js
@@ -6576,14 +6576,14 @@ $.mobile.document.bind( "pagecreate create", function( e ) {
 		locked: false,
 		disable: function( lock ) {
 			if ( !disabledInitially && !$.mobile.zoom.locked ) {
-				//meta.attr( "content", disabledZoom );
+				meta.attr( "content", disabledZoom );
 				$.mobile.zoom.enabled = false;
 				$.mobile.zoom.locked = lock || false;
 			}
 		},
 		enable: function( unlock ) {
 			if ( !disabledInitially && ( !$.mobile.zoom.locked || unlock === true ) ) {
-				//meta.attr( "content", enabledZoom );
+				meta.attr( "content", enabledZoom );
 				$.mobile.zoom.enabled = true;
 				$.mobile.zoom.locked = false;
 			}
